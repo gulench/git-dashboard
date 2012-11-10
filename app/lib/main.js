ccccffd51a54c469275aea865bfd803582d1e9d5
Gulen Chongtham <gulench@gmail.com>
2012-11-11 02:12:22 +0530

    v0.4 changes; introducing router

--- app/lib/main.js
+++ app/lib/main.js
@@ -1,21 +1,8 @@
 require('dashboard/core');
 // require('dashboard/store');
 require('dashboard/github_data_source');
-//require('dashboard/router');
+require('dashboard/router');
 require('dashboard/controllers');
 require('dashboard/views');
 
-Dashboard.gitHubDataSource = Dashboard.GitHubDataSource.create();
-
-Dashboard.set('repositoriesController', Dashboard.RepositoriesController.create({
-  content: [],
-  dataSourceBinding: 'Dashboard.gitHubDataSource'
-}));
-Dashboard.get('repositoriesController').loadWatchedRepositories('pangratz');
-
-Ember.View.create({
-  templateName: 'repositories',
-  controllerBinding: 'Dashboard.repositoriesController'
-}).append();
-
-//Dashboard.initialize();
+Dashboard.initialize();
--- app/lib/router.js
+++ app/lib/router.js
@@ -1,11 +1,25 @@
 require('dashboard/core');
+require('dashboard/github_data_source');
 
 Dashboard.Router = Ember.Router.extend({
-  enableLogging: true,
+  //- enableLogging: true,
 
   root: Ember.Route.extend({
     index: Ember.Route.extend({
-      route: '/'
+      route: '/',
+      connectOutlets: function(router) {
+        var dataSource = Dashboard.GitHubDataSource.create();
+
+        var repositoriesController = router.get('repositoriesController');
+        repositoriesController.set('dataSource', dataSource);
+        repositoriesController.set('content', []);
+        repositoriesController.loadWatchedRepositories('pagratz');
+
+        Ember.View.create({
+          templateName: 'repositories',
+          controller: repositoriesController
+        }).append();
+      }
     })
   })
 });
